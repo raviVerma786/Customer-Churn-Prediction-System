1b0908d6a0689ae1a5cd99577676203dff91f2aa
Ravi Verma <97225196+raviVerma786@users.noreply.github.com>
2026-04-24 16:10:34 +0530

    update text

--- app.py
+++ app.py
@@ -74,9 +74,7 @@ st.markdown("""
     .stStatusWidget {visibility: hidden !important;}
     iframe[title="streamlit_analytics"] {display: none !important;}
     [data-testid="stToolbar"] {background-color: #36454F !important;}
-    .stApp {color:grey;}
-    .st-bo {color: #525252}
-    button {color: black}
+    .stApp {color: #636363}
 </style>
 """, unsafe_allow_html=True)
 
